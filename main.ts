e4448b22a1507277fce32bb0e85f1aa2471c8786
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-10-10 16:52:30 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,48 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Campbell
+ * Created on: Oct 2025
+ * This program shows an if statement
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = -1
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.onGesture(Gesture.Shake, function() {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // if randomNumber == 0
+    if (randomNumber == 0) {
+    basic.showIcon(IconNames.SmallSquare)
+    }
+    
+    // if randomNumber == 1
+    if (randomNumber == 1) {
+    basic.showIcon(IconNames.Chessboard)
+    }
+
+    // if randomNumber == 2
+    if (randomNumber == 2) {
+    basic.showIcon(IconNames.Scissors)    
+    }
+
+    // Pause to show you're redy again
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+
+    // Adds 1 to score
+    input.onButtonPressed(Button.A, function () {
+    randomNumber = (randomNumber + 1)
+    basic.clearScreen()
+    }
+
+    //input.onButtonPressed(Button.B, function () {
+    //basic.showString('' + (randomNumber).toString())
+    //}
+   
+})
